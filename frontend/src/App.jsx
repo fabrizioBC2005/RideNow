ceff9a8f59e81faa49678ada9f0effefedbc9041
fabrizio Coding <fabrizio23enero@gmai.com>
2026-04-26 13:05:48 -0500

    modificando las rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import NegociosPage from './pages/NegociosPage'
+import QuienesSomosPage from './pages/QuienesSomosPage'
+import ViajePage from './pages/ViajePage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/"             element={<HomePage />} />
+      <Route path="/negocios"     element={<NegociosPage />} />
+      <Route path="/quienes-somos" element={<QuienesSomosPage />} />
+      <Route path="/viaje"        element={<ViajePage />} />
     </Routes>
   )
 }
